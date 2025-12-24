2f183342dadad5d635e89a8f3a167b8f5c188032
ulalume <ulalume.dev@gmail.com>
2025-12-24 15:38:52 +0900

    Add initial Tree-sitter grammar for ctrmml
    Introduce grammar definition, parser, highlighting, and configuration files for the ctrmml MML language. Includes grammar.js, grammar.json, parser.c, highlights.scm, node-types.json, and supporting package/config files.

---
+++ grammar.js
@@ -0,0 +1,60 @@
+module.exports = grammar({
+  name: "ctrmml",
+
+  extras: ($) => [/\s/, $.comment],
+
+  rules: {
+    source_file: ($) => repeat($._item),
+
+    _item: ($) =>
+      choice(
+        $.meta_command,
+        $.at_command,
+        $.instrument_type,
+        $.note,
+        $.rest,
+        $.command_with_number,
+        $.command,
+        $.escape_command,
+        $.number,
+        $.string,
+        $.track_selector,
+        $.operator,
+        $.punctuation,
+        $.param_key,
+      ),
+
+    meta_command: ($) =>
+      seq(
+        $.meta_keyword,
+        optional(choice($.meta_platform_value, $.meta_value)),
+      ),
+    meta_keyword: ($) =>
+      token(
+        /#(?:title|composer|author|date|comment|platform|option|game|composerj|programmer)/,
+      ),
+    meta_platform_value: ($) => token(/\s+(?:megadrive|mdsdrv)[^\n]*/),
+    meta_value: ($) => token(/\s+[^\n]*/),
+    at_command: ($) => token(/@(?:[A-Za-z]\d+|\d+)/),
+
+    instrument_type: ($) => token(/(?:fm|psg|pcm|2op)/),
+    note: ($) => token(/(?:[a-gh](?:[+\-=])?)+/),
+    rest: ($) => token(/r/),
+
+    command_with_number: ($) =>
+      token(/(?:o|l|q|Q|C|R|s|t|T|v|V|p|k|K|E|M|P|G|D|_+)[+-]?\d+(?:\.\d+)?/),
+    command: ($) => token(/(?:o|l|q|s|t|v|p|k|__|_|\^|&)/),
+    escape_command: ($) => token(/\\=?/),
+
+    number: ($) => token(/[+-]?\d+(?:\.\d+)?/),
+    string: ($) => token(/"(?:[^"\\]|\\.)*"|'(?:[^'\\]|\\.)*'/),
+
+    track_selector: ($) => token(/(?:\*[0-9]+|[A-Z]+)/),
+
+    operator: ($) => token(/[+\-*\/=<>:()]/),
+    punctuation: ($) => token(/[\[\]\{\}\|.,]/),
+    param_key: ($) => token(/(?:rate|offset)/),
+
+    comment: ($) => token(/;[^\n]*/),
+  },
+});
